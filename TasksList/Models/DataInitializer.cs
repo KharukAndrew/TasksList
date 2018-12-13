1e503990f9c31e56bad197de8c8d55f06788f133
Andrew Kharuk <haruk1972@gmail.com>
2018-12-13 20:45:11 +0500

    Небольшие корректировки вывода списка задач и инициализации БД.

--- TasksList/Models/DataInitializer.cs
+++ TasksList/Models/DataInitializer.cs
@@ -4,13 +4,17 @@ using System.Data.Entity;
 
 namespace TasksList.Models
 {
-    public class DataInitializer : DropCreateDatabaseAlways<TasksListDb>
+    public class DataInitializer : DropCreateDatabaseIfModelChanges<TasksListDb>
     {
         protected override void Seed(TasksListDb db)
         {
             db.TaskEntities.AddRange(new List<TaskEntity>
             {
-                new TaskEntity{Date =DateTime.Now.AddDays(-1), Header = "Test", Text = "This is text forthe test task", IsDone = false }
+                new TaskEntity{Date =DateTime.Now.AddDays(-5), Header = "One task", Text = "This is text for the one task", IsDone = true },
+                new TaskEntity{Date =DateTime.Now.AddDays(-4), Header = "Two task", Text = "This is text for the two task", IsDone = false },
+                new TaskEntity{Date =DateTime.Now.AddDays(-3), Header = "Three task", Text = "This is text for the three task", IsDone = true },
+                new TaskEntity{Date =DateTime.Now.AddDays(-2), Header = "Four task", Text = "This is text for the four task", IsDone = false },
+                new TaskEntity{Date =DateTime.Now.AddDays(-1), Header = "Five task", Text = "This is text for the five task", IsDone = false }
             });
 
             base.Seed(db);
