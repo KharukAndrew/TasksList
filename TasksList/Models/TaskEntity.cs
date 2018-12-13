0e925dff09e858e683767d61c067fd820dbfd604
Andrew Kharuk <haruk1972@gmail.com>
2018-12-13 19:17:33 +0500

    Валидация модели на сервере.

--- TasksList/Models/TaskEntity.cs
+++ TasksList/Models/TaskEntity.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TasksList.Models
 {
     public class TaskEntity
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter Date")]
+        [DataType(DataType.Date)]
         public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Please enter Header")]
         public string Header { get; set; }
+
+        [Required(ErrorMessage = "Please enter Text")]
+        [DataType(DataType.Text)]
         public string Text { get; set; }
         public bool IsDone { get; set; }
     }
